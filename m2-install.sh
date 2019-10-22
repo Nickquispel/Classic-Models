5cfb73617c1b3bb2d6c729ca4eb8604a2910f090
Nick Quispel <n.quispel@xsarus.nl>
2019-10-22 12:11:30 +0200

    2.1 installeer Magento (installatie via composer, setup draaien, Basis inrichting van de shop goed zetten)

---
+++ m2-install.sh
@@ -0,0 +1,3 @@
+php72 bin/magento setup:install --base-url=https://classic-models.nql-72.at.xsar.us/ --db-host=mysql57-2 --db-name=classic_models --db-user=xsarus --db-password=xsarus --admin-firstname=admin --admin-lastname=admin --admin-email=admin@hotmail.com --admin-user=admin --admin-password=Heineken!!1 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1
+
+
