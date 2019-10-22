b54d375a4e353e3eaba835478fed218ca0138a13
Nick Quispel <n.quispel@xsarus.nl>
2019-10-22 14:28:33 +0200

    2.2 Maak een eigen thema, themanaam: classicmodels, parent thema: magento base, alternatief logo, frontend aanpassing via less / css

---
+++ app/design/frontend/Xsarus/classicmodels/themes.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: area, one of (frontend|adminhtml|doc),
+ * name: theme name in format Vendor/theme-name,
+ * locale: locale,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+    dlk: {
+        area: 'frontend',
+        name: 'Xsarus/classicmodels',
+        locale: 'nl_NL',
+        files: [
+            'css/site',
+        ],
+        dsl: 'less'
+    },
+    backend: {
+        area: 'adminhtml',
+        name: 'Magento/backend',
+        locale: 'en_US',
+        files: [
+            'css/styles-old',
+            'css/styles'
+        ],
+        dsl: 'less'
+    }
+};
